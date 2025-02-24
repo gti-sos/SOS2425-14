fc55693ca9dba637055f73b61c063e78961d0a1b
Frarodmar19 <frarodmar19@alum.us.es>
2025-02-24 23:26:19 +0100

    estructured proyect

---
+++ src/js/main/navbar.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function navbar(){
+    window.addEventListener('scroll', function() {
+        const navbar = document.getElementById('navbar');
+        if (window.scrollY > 0) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', navbar);
